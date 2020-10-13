3b86b6f67bad14874abe7e0bc1a23d8b290e1839
joao <joao20014@gmail.com>
2020-10-13 21:53:50 +0100

    Added Mutex Lock for removeCommand()

--- main.c
+++ main.c
@@ -16,6 +16,8 @@ char inputCommands[MAX_COMMANDS][MAX_INPUT_SIZE];
 int numberCommands = 0;
 int headQueue = 0;
 
+pthread_mutex_t commandsMutex = PTHREAD_MUTEX_INITIALIZER;
+
 void validateInitArgs(int argc, char *argv[])
 {
 	/* Usage: ./tecnicofs inputfile outputfile numthreads synchstrategy */
@@ -83,7 +85,6 @@ int insertCommand(char *data)
 	return 0;
 }
 
-// Critical Zone
 char *removeCommand()
 {
 	if (numberCommands > 0)
@@ -144,82 +145,58 @@ void processInput(FILE *commands_file)
 	}
 }
 
-void applyCommand(const char *command)
-{
-	char token, type;
-	char name[MAX_INPUT_SIZE];
-	int numTokens = sscanf(command, "%c %s %c", &token, name, &type);
-	if (numTokens < 2)
-	{
-		fprintf(stderr, "Error: invalid command in Queue\n");
-		exit(EXIT_FAILURE);
-	}
-	int searchResult;
-	switch (token)
-	{
-	case 'c':
-		switch (type)
-		{
-		case 'f':
-			printf("Create file: %s\n", name);
-			create(name, T_FILE);
-			break;
-		case 'd':
-			printf("Create directory: %s\n", name);
-			create(name, T_DIRECTORY);
-			break;
-		default:
-			fprintf(stderr, "Error: invalid node type\n");
-			exit(EXIT_FAILURE);
-		}
-		break;
-	case 'l':
-		searchResult = lookup(name);
-		if (searchResult >= 0)
-			printf("Search: %s found\n", name);
-		else
-			printf("Search: %s not found\n", name);
-		break;
-	case 'd':
-		printf("Delete: %s\n", name);
-		delete (name);
-		break;
-	default:
-	{
-		fprintf(stderr, "Error: command to apply\n");
-		exit(EXIT_FAILURE);
-	}
-	}
-}
-
-void *queueWorker(char *syncstrat)
+void *queueWorker(void *syncstrat)
 {
 	while (numberCommands > 0)
 	{
+		pthread_mutex_lock(&commandsMutex);
 		const char *command = removeCommand();
-		applyCommand(command);
+		pthread_mutex_unlock(&commandsMutex);
+		char token, type;
+		char name[MAX_INPUT_SIZE];
+		int numTokens = sscanf(command, "%c %s %c", &token, name, &type);
+		if (numTokens < 2)
+		{
+			fprintf(stderr, "Error: invalid command in Queue\n");
+			exit(EXIT_FAILURE);
+		}
+		int searchResult;
+		switch (token)
+		{
+		case 'c':
+			switch (type)
+			{
+			case 'f':
+				printf("Create file: %s\n", name);
+				create(name, T_FILE);
+				break;
+			case 'd':
+				printf("Create directory: %s\n", name);
+				create(name, T_DIRECTORY);
+				break;
+			default:
+				fprintf(stderr, "Error: invalid node type\n");
+				exit(EXIT_FAILURE);
+			}
+			break;
+		case 'l':
+			searchResult = lookup(name);
+			if (searchResult >= 0)
+				printf("Search: %s found\n", name);
+			else
+				printf("Search: %s not found\n", name);
+			break;
+		case 'd':
+			printf("Delete: %s\n", name);
+			delete (name);
+			break;
+		default:
+		{
+			fprintf(stderr, "Error: command to apply\n");
+			exit(EXIT_FAILURE);
+		}
+		}
 	}
-	// switch (syncstrat[0])
-	// {
-	// // mutex
-	// case 'm':
-
-	// 	break;
-
-	// rwlock
-	// case 'r':
-
-	// 	break;
-
-	// nosync
-	// case 'n':
-
-	// 	break;
-
-	// default:
-	// 	break;
-	// }
-
 	return NULL;
 }
 
@@ -228,23 +205,19 @@ void generateThreads(char *threads_count_char, char *syncstrat)
 	int i, *result;
 	int threads_count = atoi(threads_count_char);
 	pthread_t tid[threads_count];
-	/* pthread_t *tid = (pthread_t *)malloc(sizeof(pthread_t) * threads_count); */
 	for (i = 0; i < threads_count; i++)
 	{
-		if (pthread_create(&tid[i], NULL, queueWorker, syncstrat) != 0) /* WIP Synchcoiso */
-			fprintf(stderr, "Failed to create thread number %d.\n", i);
-		printf("Thread number %d has been successfully created.\n", i);
+		if (pthread_create(&tid[i], NULL, queueWorker, syncstrat) != 0)
+			fprintf(stderr, "Failed to create thread %d.\n", i);
 	}
 	for (i = 0; i < threads_count; i++)
 	{
-		// WIP (perceber o mambo do &result)
 		if (pthread_join(tid[i], (void **)&result) != 0)
 			printf("Error while joining thread.\n");
-		printf("Thread has finished %d\n", i);
 	}
 }
 
-double getTimeDiff(struct timeval *begin, struct timeval *end)
+double timeDiff(struct timeval *begin, struct timeval *end)
 {
 	long seconds = end->tv_sec - begin->tv_sec;
 	long microseconds = end->tv_usec - begin->tv_usec;
@@ -252,29 +225,22 @@ double getTimeDiff(struct timeval *begin, struct timeval *end)
 	return elapsed;
 }
 
-/* Usage: ./tecnicofs intputfile outputfile nthreads synchstrategy */
 int main(int argc, char *argv[])
 {
 	struct timeval begin, end;
 	FILE *file_buffer;
 	gettimeofday(&begin, 0);
-	/* Pre-validating input arguments */
 	validateInitArgs(argc, argv);
-	/* init filesystem */
 	init_fs();
-	/* process input and print tree */
 	file_buffer = fopenSafe(argv[1], "r");
 	processInput(file_buffer);
 	fcloseSafe(file_buffer);
-	stdout = fopenSafe(argv[2], "w");
-	/* generating threads */
+	file_buffer = fopenSafe(argv[2], "w");
 	generateThreads(argv[3], argv[4]);
-	print_tecnicofs_tree(stdout);
-	/* release allocated memory */
+	print_tecnicofs_tree(file_buffer);
 	destroy_fs();
-	/* get final time and perform difference calculations */
 	gettimeofday(&end, 0);
-	fprintf(file_buffer, "TecnicoFS completed in %.4f seconds.\n", getTimeDiff(&begin, &end));
-	fcloseSafe(stdout);
+	fprintf(file_buffer, "TecnicoFS completed in %.4f seconds.\n", timeDiff(&begin, &end));
+	fcloseSafe(file_buffer);
 	exit(EXIT_SUCCESS);
 }
