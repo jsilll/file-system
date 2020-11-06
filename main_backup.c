9dbbf63a19fd48764fdd198ee1b7cadb3c40cfe1
joao <joao20014@gmail.com>
2020-11-06 11:57:49 +0000

    Requesito 2 almost done

--- main_backup.c
+++ main_backup.c
@@ -8,16 +8,15 @@
 #include <unistd.h>
 #include <pthread.h>
 
-#define MAX_COMMANDS 10
+#define MAX_COMMANDS 150000
 #define MAX_INPUT_SIZE 100
 
 int numberThreads = 0;
 char inputCommands[MAX_COMMANDS][MAX_INPUT_SIZE];
+int numberCommands = 0;
+int headQueue = 0;
 
-int prodptr = 0, consptr = 0, count = 0;
 pthread_mutex_t commandsMutex;
-pthread_cond_t podeProduzir, podeConsumir;
-int endoffile = 0;
 
 void commandsLock()
 {
@@ -71,40 +70,30 @@ FILE *fopenSafe(char *file_name, const char *mode)
 int fcloseSafe(FILE *file)
 {
 	if (fclose(file) == EOF)
+	{
 		fprintf(stderr, "Error closing a file.\n");
+	}
 	return 0;
 }
 
-// int getNextSlot()
-// {
-// 	int index = bufferTail;
-// 	while (inputCommands[(index++) % MAX_COMMANDS][0] != DELETED_COMMAND)
-// 	{
-// 	}
-// 	return index;
-// }
-
-void insertCommand(char *data)
+int insertCommand(char *data)
 {
-	while (count == MAX_COMMANDS)
-		pthread_cond_wait(&podeProduzir, &commandsMutex);
-
-	strcpy(inputCommands[prodptr], data);
-	prodptr++;
-	if (prodptr == MAX_COMMANDS)
-		prodptr = 0;
-	count++;
-
-	pthread_cond_signal(&podeConsumir);
+	if (numberCommands != MAX_COMMANDS)
+	{
+		strcpy(inputCommands[numberCommands++], data);
+		return 1;
+	}
+	return 0;
 }
 
 char *removeCommand()
 {
-	char *res = inputCommands[consptr++];
-	if (consptr == MAX_COMMANDS)
-		consptr = 0;
-	count--;
-	return res;
+	if (numberCommands > 0)
+	{
+		numberCommands--;
+		return inputCommands[headQueue++];
+	}
+	return NULL;
 }
 
 void errorParse()
@@ -113,46 +102,47 @@ void errorParse()
 	exit(EXIT_FAILURE);
 }
 
-void *commandsProvider(void *commands_file) //Produtor
+void processInput(FILE *commands_file)
 {
 	char line[MAX_INPUT_SIZE];
 	int n = sizeof(line) / sizeof(char);
 	/* break loop with ^Z or ^D */
-	while (fgets(line, n, (FILE *)commands_file))
+	while (fgets(line, n, commands_file))
 	{
 		char token, type;
 		char name[MAX_INPUT_SIZE];
 		int numTokens = sscanf(line, "%c %s %c", &token, name, &type);
-
 		/* perform minimal validation */
 		if (numTokens < 1)
+		{
 			continue;
-
+		}
 		switch (token)
 		{
 		case 'c':
 			if (numTokens != 3)
 				errorParse();
-			insertCommand(line);
-			break;
+			if (insertCommand(line))
+				break;
+			return;
 		case 'l':
 			if (numTokens != 2)
 				errorParse();
-			insertCommand(line);
-			break;
+			if (insertCommand(line))
+				break;
+			return;
 		case 'd':
 			if (numTokens != 2)
 				errorParse();
-			insertCommand(line);
-			break;
+			if (insertCommand(line))
+				break;
+			return;
 		case '#':
 			break;
 		default:
 			errorParse();
 		}
 	}
-	pthread_cond_signal(&podeConsumir);
-	return NULL;
 }
 
 void *queueWorker()
@@ -160,93 +150,84 @@ void *queueWorker()
 	while (1)
 	{
 		commandsLock();
-		while (count == 0 && !endoffile)
-			pthread_cond_wait(&podeConsumir, &commandsMutex);
-
-		if (count == 0 && endoffile)
+		if (numberCommands > 0)
 		{
-			pthread_cond_signal(&podeConsumir);
+			/* Read the new command */
+			const char *command = removeCommand();
 			commandsUnlock();
-			return;
-		}
 
-		const char *command = removeCommand();
-		char token, type;
-		char name[MAX_INPUT_SIZE];
-		int numTokens = sscanf(command, "%c %s %c", &token, name, &type);
-
-		pthread_cond_signal(&podeProduzir);
-		commandsUnlock();
-
-		if (numTokens < 2)
-		{
-			fprintf(stderr, "Error: invalid command in Queue\n");
-			exit(EXIT_FAILURE);
-		}
-		int searchResult;
-		switch (token)
-		{
-		case 'c':
-			switch (type)
+			char token, type;
+			char name[MAX_INPUT_SIZE];
+			int numTokens = sscanf(command, "%c %s %c", &token, name, &type);
+			if (numTokens < 2)
 			{
-			case 'f':
-				printf("Create file: %s\n", name);
-				create(name, T_FILE);
-				break;
-			case 'd':
-				printf("Create directory: %s\n", name);
-				create(name, T_DIRECTORY);
-				break;
-			default:
-				fprintf(stderr, "Error: invalid node type\n");
+				fprintf(stderr, "Error: invalid command in Queue\n");
 				exit(EXIT_FAILURE);
 			}
-			break;
-		case 'l':
-			searchResult = lookup(name);
-			if (searchResult >= 0)
-				printf("Search: %s found\n", name);
-			else
-				printf("Search: %s not found\n", name);
-			break;
-		case 'd':
-			printf("Delete: %s\n", name);
-			delete (name);
-			break;
-		default:
-		{
-			fprintf(stderr, "Error: command to apply\n");
-			exit(EXIT_FAILURE);
+			int searchResult;
+			switch (token)
+			{
+			case 'c':
+				switch (type)
+				{
+				case 'f':
+					printf("Create file: %s\n", name);
+					create(name, T_FILE);
+					break;
+				case 'd':
+					printf("Create directory: %s\n", name);
+					create(name, T_DIRECTORY);
+					break;
+				default:
+					fprintf(stderr, "Error: invalid node type\n");
+					exit(EXIT_FAILURE);
+				}
+				break;
+			case 'l':
+				searchResult = lookup(name);
+				if (searchResult >= 0)
+					printf("Search: %s found\n", name);
+				else
+					printf("Search: %s not found\n", name);
+				break;
+			case 'd':
+				printf("Delete: %s\n", name);
+				delete (name);
+				break;
+			default:
+			{
+				fprintf(stderr, "Error: command to apply\n");
+				exit(EXIT_FAILURE);
+			}
+			}
 		}
+		else
+		{
+			/* There's no more commands to read, then return */
+			commandsUnlock();
+			break;
 		}
 	}
+	return NULL;
 }
 
-void executeThreads(char *threads_count_char, FILE *commands_file)
+void executeThreads(char *threads_count_char)
 {
 	int i, *result;
 	int threads_count = atoi(threads_count_char);
-	pthread_t command_thread, worker_tid[threads_count];
-
-	/* Creating command thread */
-	if (pthread_create(&command_thread, NULL, commandsProvider, (void *)commands_file) != 0)
-		fprintf(stderr, "Failed to commands thread.\n");
-
-	/* Creating the consumer threads */
+	pthread_t tid[threads_count];
+	pthread_mutex_init(&commandsMutex, NULL);
 	for (i = 0; i < threads_count; i++)
 	{
-		if (pthread_create(&worker_tid[i], NULL, queueWorker, NULL) != 0)
+		if (pthread_create(&tid[i], NULL, queueWorker, NULL) != 0)
 			fprintf(stderr, "Failed to create thread %d.\n", i);
 	}
-
-	if (pthread_join(command_thread, (void **)&result) != 0)
-		printf("Error while joining thread.\n");
-
 	for (i = 0; i < threads_count; i++)
 	{
-		if (pthread_join(worker_tid[i], (void **)&result) != 0)
+		if (pthread_join(tid[i], (void **)&result) != 0)
 			printf("Error while joining thread.\n");
 	}
+	pthread_mutex_destroy(&commandsMutex);
 }
 
 double timeDiff(struct timeval *begin, struct timeval *end)
@@ -265,11 +246,12 @@ int main(int argc, char *argv[])
 	validateInitArgs(argc, argv);
 	init_fs();
 
-	gettimeofday(&begin, 0);
-
 	file_buffer = fopenSafe(argv[1], "r");
-	executeThreads(argv[3], file_buffer);
+	processInput(file_buffer);
 	fcloseSafe(file_buffer);
+
+	gettimeofday(&begin, 0);
+	executeThreads(argv[3]);
 
 	file_buffer = fopenSafe(argv[2], "w");
 	print_tecnicofs_tree(file_buffer);
