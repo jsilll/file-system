4b059ba5eeccc749a4c10ab61a3152e7e1ad39cb
joao <joao20014@gmail.com>
2020-11-11 11:49:38 +0000

    Shell Script Done

--- runTests.sh
+++ runTests.sh
@@ -4,16 +4,33 @@ INPUTDIR=$1
 OUTPUTDIR=$2
 MAXTHREADS=$3
 CURRENTTHREADS=1
+    
+if [ ! -d ${INPUTDIR%/} ]
+then
+    echo "Diretoria de input inexistente.";
+    exit 1;
+fi
 
-make clean
-make
+if [ ! $MAXTHREADS -gt 0 ]
+then
+    echo "Mínimo número de threads é 1.";
+    exit 1;
+fi
 
-for x in $INPUTDIR/*.txt; 
+if [ ! -d ${OUTPUTDIR%/} ]
+then
+    echo "Diretoria de output inexistente."
+    return 1;
+fi
+
+for x in ${INPUTDIR%/}/*.txt; 
 do  
-    for t in $MAXTHREADS;
+    basename="$(basename -- $x)";
+    for t in $(seq 1 $MAXTHREADS);
     do
-    exec ./tecnicofs $x $OUTPUTDIR/$x.out $t | grep "completed"
+    echo InputFile=$basename NumThreads=$t;
+    exec ./tecnicofs $x ${OUTPUTDIR%/}/${basename%.*}-$t.txt $t | grep "completed";
     done
 done
 
-echo "Done."
+exit 1;
