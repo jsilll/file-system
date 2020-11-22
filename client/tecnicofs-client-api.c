ee89e17b6fd567d4daad8b679339a3b6a0f00db4
jsilll <joaos20014@gmail.com>
2020-11-22 13:42:11 +0000

    Updated Base Setup

--- client/tecnicofs-client-api.c
+++ client/tecnicofs-client-api.c
@@ -1,31 +1,19 @@
 #include "tecnicofs-client-api.h"
-#include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <sys/un.h>
-#include <stdio.h>
+#include <unistd.h>
 
-int tfsCreate(char *filename, char nodeType) {
-  return -1;
-}
+int tfsCreate(char *filename, char nodeType) { return -1; }
 
-int tfsDelete(char *path) {
-  return -1;
-}
+int tfsDelete(char *path) { return -1; }
 
-int tfsMove(char *from, char *to) {
-  return -1;
-}
+int tfsMove(char *from, char *to) { return -1; }
 
-int tfsLookup(char *path) {
-  return -1;
-}
+int tfsLookup(char *path) { return -1; }
 
-int tfsMount(char * sockPath) {
-  return -1;
-}
+int tfsMount(char *sockPath) { return -1; }
 
-int tfsUnmount() {
-  return -1;
-}
+int tfsUnmount() { return -1; }
